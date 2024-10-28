75eee4fae3c91b55184f5936013f611bcf158ca8
Robbie Fraser <robbie.fraser@students.codeinstitute.net>
2024-10-28 06:30:40 +0000

    Add more javascript for memory card game

--- memory.js
+++ memory.js
@@ -14,13 +14,43 @@ var cardList = [
 
 var cardSet;
 var board = [];
+var rows = 4;
+var columns = 5;
 
-windown.onload = function() {
+window.onload = function() {
     shuffleCards();
     startGame();
 }
 
 function shuffleCards() {
+    //this creates two of each card
     cardSet = cardList.concat(cardList);
     console.log(cardSet);
+    //this then shuffles the cards
+    for (let i = 0; i < cardSet.length; i++) {
+        let c = Math.floor(Math.random() * cardSet.length);
+        // we then swap the cards around
+        let swap = cardSet[i];
+        cardSet[i] = cardSet[c];
+        cardSet[c] = swap;
+    }
+    console.log(cardSet);
+}
+
+function startGame() {
+    // creating the board
+    for (let r = 0; r < rows; r++) {
+        let row = [];
+        for (let c = 0; c < columns; c++) {
+            let cardImage = cardSet.pop();
+            row.push(cardImage);
+
+            //creates the img, class and gives it a source
+            let card = document.createElement("img");
+            card.id = r.toString() + "-" + c.toString();
+            card.src = cardImage + ".avif";
+            card.classList("card");
+            document.getElementById("game-board").append(card);
+        }
+    }
 }
