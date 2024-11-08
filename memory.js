0315b55a25d8204478efb9cbafe205d51f66ab54
Robbie Fraser <robbie.fraser@students.codeinstitute.net>
2024-11-08 06:19:31 +0000

    Add new background image, add modal for memory card game, add HTML structure to pokemon-battle

--- memory.js
+++ memory.js
@@ -1,3 +1,22 @@
+//Code for help modal
+var modal = document.getElementById("help-modal");
+var btn = document.getElementById("help-button");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
 var cardList = [
     "bulbasaur",
     "charizard",
